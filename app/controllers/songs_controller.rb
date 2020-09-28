2af8dc6edefa4b83800411dd62257a24ad349f6d
gerardmi24 <gerardmistretta@yahoo.com>
2020-09-27 21:58:14 -0400

    Not finished

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,27 @@
+class SongsController < ApplicationController
+  
+  def index
+    @songs = Song.all
+  end
+
+  def show
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+
+end
